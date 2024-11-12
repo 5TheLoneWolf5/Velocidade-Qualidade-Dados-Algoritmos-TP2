870909dbb379cf34785b88d87ef090188bf3c969
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2024-11-12 00:20:47 -0300

    Third commit

--- Ex02.py
+++ Ex02.py
@@ -1,7 +1,18 @@
-
 """
 
 A complexidade de tempo desse algoritmo de ordenação (Quicksort) de é O(n log n) no melhor caso, e O(n^2) no pior caso.
 
 """
 
+import random
+
+def partition(list):
+   pass
+
+def quick_sort(list, low=0, high=None):
+   pass 
+
+
+data = [int(random.random() * 100) for _ in range(100)]
+
+quick_sort(data)
--- Ex03.py
+++ Ex03.py
@@ -1,9 +1,24 @@
+import random
+
 def primeiro_algoritmo(list):
-    for i in list:
-        for j in i:
-            if i == j:
-                print("Número duplicado encontrado!")
+    length = len(list)
+    for i in range(length):
+        for j in range(length):
+            if list[i] == list[j]:
+                print(f"Número duplicado encontrado! {list[i]}")
 
-def segundo_algoritmo():
-    pass
+def segundo_algoritmo(list):
+    length = len(list)
+    for i in range(length):
+        for j in range(i, length):
+            if list[i] == list[j]:
+                print(f"Número duplicado encontrado! {list[i]}")
 
+listNumbers = [int(random.random() * 100) for _ in range(100)]
+
+print(listNumbers)
+
+print("### Primeiro Algoritmo ###")
+primeiro_algoritmo(listNumbers)
+print("### Segundo Algoritmo ###")
+segundo_algoritmo(listNumbers)
