1dcdda7288bc313f35af95edab5fcf2834f87ef0
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2024-11-12 18:49:22 -0300

    TP2.

--- Ex03.py
+++ Ex03.py
@@ -10,27 +10,11 @@ def primeiro_algoritmo(list):
 def segundo_algoritmo(list):
     
     length = len(list)
-    listForSearch = []
-
-    def binary_search(array, value):
-        first = 0
-        last = len(array) - 1
-
-        for item in range(0, len(array)):
-            middlePoint = (first + last) // 2
-
-            if (value == array[middlePoint]):
-                print(f"Número duplicado encontrado! {array[item]}")
-            else:
-                if (value > array[middlePoint]):
-                    first = middlePoint + 1
-                else:
-                    last = middlePoint - 1
-
-        return -1
 
     for i in range(length):
-        binary_search(list, list[i])
+        for j in range(i, length):
+            if list[i] == list[j]:
+                print(f"Número duplicado encontrado! {list[i]}")
 
 listNumbers = [int(random.random() * 100) for _ in range(50)]
 
